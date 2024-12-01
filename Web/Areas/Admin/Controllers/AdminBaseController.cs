eb6823009219c78b78550cd700e1b48650966014
Arefkhooraj <aref.khooraj80@gmail.com>
2024-12-01 12:19:24 +0330

    Add Admin Index Tempelate And References

---
+++ Web/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminBaseController : Controller
+    {
+    }
+}
---
+++ Web/Areas/Admin/ViewComponents/AdminSideBarViewComponent/AdminSideBarViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Areas.Admin.ViewComponents.AdminSideBarViewComponent
+{
+    public class AdminSideBarViewComponent:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("AdminSideBar");
+        }
+    }
+}
