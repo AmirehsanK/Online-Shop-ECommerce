e07d034291921abe0b27677c724b58ffb6e77e3d
ArefKhooraj <Aref.khooraj80@gmail.com>
2025-01-12 16:55:04 +0330

    AddSome Photo And Slider

---
+++ Web/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Areas.Admin.Controllers
+{
+    public class OrderController : AdminBaseController
+    {
+        [HttpGet]
+        public IActionResult UserOrdersList()
+        {
+            return View();
+        }
+    }
+}
